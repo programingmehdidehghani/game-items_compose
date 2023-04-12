6afaf93f4322af997e78cde0a0f0f57631a6c398
mehdi <dehghani_m@rocketmail.com>
2023-04-12 22:14:59 +0430

    make contants

---
+++ app/src/main/java/com/example/game_api_compose/common/Constants.kt
@@ -0,0 +1,13 @@
+package com.example.game_api_compose.common
+
+object Constants {
+
+    const val BASE_URL = "https://valorant-api.com/"
+    const val CATEGORY_AGENTS = "Agents"
+    const val CATEGORY_MAPS = "Maps"
+    const val CATEGORY_WEAPONS = "Weapons"
+    const val CATEGORY_COMPETITIVE_TIERS = "Tiers"
+    const val PARAM_AGENT_ID = "agentUuid"
+    const val PARAM_MAP_ID = "mapUuid"
+    const val PARAM_WEAPON_ID = "weaponUuid"
+}
