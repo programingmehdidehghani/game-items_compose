755dd7a2fbd431d642869c121f34387b132d8d08
mehdi <dehghani_m@rocketmail.com>
2023-04-12 23:24:29 +0430

    finsh class searchBar

--- app/src/main/java/com/example/game_api_compose/common/components/SearchBar.kt
+++ app/src/main/java/com/example/game_api_compose/common/components/SearchBar.kt
@@ -1,10 +1,24 @@
 package com.example.game_api_compose.common.components
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
 import com.example.game_api_compose.presentation.theme.ValoRed
 
@@ -15,9 +29,73 @@ fun SearchBar(
    onSearchTextChanged: (String) -> Unit = {},
    border: BorderStroke = BorderStroke((0.5f).dp, ValoRed),
    backgroundColor: Color = MaterialTheme.colors.surface.copy(alpha = 0.7f),
-   shapes: RoundedCornerShape = RoundedCornerShape(32.dp),
+   shape: RoundedCornerShape = RoundedCornerShape(32.dp),
    onClearClick: () -> Unit = {}
 ){
-   val showClearBottom
+   var showClearButton by remember { mutableStateOf(false) }
+   val focusManager = LocalFocusManager.current
+
+   Card(
+      elevation = 8.dp,
+      modifier = Modifier.padding(12.dp),
+      border = border,
+      backgroundColor = backgroundColor,
+      shape = shape
+   ) {
+      Row(verticalAlignment = Alignment.CenterVertically) {
+         
+         Icon(
+            modifier = Modifier.padding(start = 12.dp),
+            imageVector = Icons.Filled.Search,
+            contentDescription = "Search",
+            tint = MaterialTheme.colors.surface
+         )
+
+         OutlinedTextField(
+            modifier = Modifier
+               .fillMaxWidth()
+               .padding(vertical = 2.dp)
+               .onFocusChanged { focusState ->
+                  showClearButton = (focusState.isFocused || searchText.isNotBlank())
+               },
+            value = searchText,
+            onValueChange = onSearchTextChanged,
+            placeholder = {
+               Text(
+                  text = placeholderText,
+                  modifier = Modifier.wrapContentHeight(),
+                  color = Color.Black
+               )
+            },
+            colors = TextFieldDefaults.textFieldColors(
+               textColor = Color.Black,
+               focusedIndicatorColor = Color.Transparent,
+               unfocusedIndicatorColor = Color.Transparent,
+               backgroundColor = Color.Transparent,
+               cursorColor = LocalContentColor.current.copy(alpha = LocalContentAlpha.current)
+            ),
+            trailingIcon = {
+               AnimatedVisibility(
+                  visible = showClearButton,
+                  enter = fadeIn(),
+                  exit = fadeOut()
+               ) {
+                  IconButton(onClick = {
+                     onClearClick()
+                     focusManager.clearFocus()
+                  }) {
+                     Icon(imageVector = Icons.Filled.Close,
+                        contentDescription = "Close")
+                  }
+                  
+               }
+            },
+            maxLines = 1,
+            singleLine = true
+         )
+
+      }
+
+   }
 
 }
