b30c861f6ae9be00d9f69b8e54e84c5cb1824f92
mehdi <dehghani_m@rocketmail.com>
2023-04-13 17:22:23 +0430

    class tierDto

--- app/src/main/java/com/example/game_api_compose/data/model/CompetitiveTier/TierDto.kt
+++ app/src/main/java/com/example/game_api_compose/data/model/CompetitiveTier/TierDto.kt
@@ -1,5 +1,31 @@
 package com.example.game_api_compose.data.model.CompetitiveTier
 
+import com.google.gson.annotations.SerializedName
+
 data class TierDto(
-    
+    @SerializedName("backgroundColor")
+    val backgroundColor: String?,
+    @SerializedName("color")
+    val color: String?,
+    @SerializedName("division")
+    val division: String?,
+    @SerializedName("divisionName")
+    val divisionName: String?,
+    @SerializedName("largeIcon")
+    val largeIcon: String?,
+    @SerializedName("rankTriangleDownIcon")
+    val rankTriangleDownIcon: Any?,
+    @SerializedName("rankTriangleUpIcon")
+    val rankTriangleUpIcon: Any?,
+    @SerializedName("smallIcon")
+    val smallIcon: String?,
+    @SerializedName("tier")
+    val tier: Int?,
+    @SerializedName("tierName")
+    val tierName: String?
+)
+
+fun TierDto.toTier() = Tier(
+    largeIcon = largeIcon,
+    tierName = tierName
 )
