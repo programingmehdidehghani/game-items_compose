83486427604d626979ac48ef2e8085f7b7ba369c
mehdi <dehghani_m@rocketmail.com>
2023-04-13 17:14:01 +0430

    class agenent response

---
+++ app/src/main/java/com/example/game_api_compose/data/model/agents/AgentResponse.kt
@@ -0,0 +1,10 @@
+package com.example.game_api_compose.data.model.agents
+
+import com.google.gson.annotations.SerializedName
+
+data class AgentResponse(
+    @SerializedName("data")
+    val data: List<AgentDto>?,
+    @SerializedName("status")
+    val status: Int?
+)
