a86cb667aa285518481885ec4bda83f98fe3448e
mehdi <dehghani_m@rocketmail.com>
2023-04-14 11:32:35 +0430

    continue repository data

--- app/src/main/java/com/example/game_api_compose/data/repository/ValorantRepositoryImpl.kt
+++ app/src/main/java/com/example/game_api_compose/data/repository/ValorantRepositoryImpl.kt
@@ -1,4 +1,27 @@
 package com.example.game_api_compose.data.repository
 
-class ValorantRepositoryImpl {
+import com.example.game_api_compose.data.model.CompetitiveTier.CompetitiveTiersResponse
+import com.example.game_api_compose.data.model.agents.AgentDetailResponse
+import com.example.game_api_compose.data.model.agents.AgentResponse
+import com.example.game_api_compose.data.model.maps.MapDetailResponse
+import com.example.game_api_compose.data.model.maps.MapsResponse
+import com.example.game_api_compose.data.model.weapons.WeaponDetailResponse
+import com.example.game_api_compose.data.model.weapons.WeaponsResponse
+import com.example.game_api_compose.data.remote.ValorantService
+import com.example.game_api_compose.domain.repository.ValorantRepository
+
+class ValorantRepositoryImpl(private val valorantService: ValorantService): ValorantRepository {
+    override suspend fun getAgents(): AgentResponse = valorantService.getAgents()
+
+    override suspend fun getAgentByUuid(agentUuid: String): AgentDetailResponse = valorantService.getAgentByUuid(agentUuid)
+
+    override suspend fun getMaps(): MapsResponse = valorantService.getMaps()
+
+    override suspend fun getMapByUuid(mapUuid: String): MapDetailResponse = valorantService.getMapByUuid(mapUuid)
+
+    override suspend fun getWeapons(): WeaponsResponse = valorantService.getWeapons()
+
+    override suspend fun getWeaponByUuid(weaponUuid: String): WeaponDetailResponse = valorantService.getWeaponByUuid(weaponUuid)
+
+    override suspend fun getCompetitiveTiers(): CompetitiveTiersResponse = valorantService.getCompetitiveTiers()
 }
