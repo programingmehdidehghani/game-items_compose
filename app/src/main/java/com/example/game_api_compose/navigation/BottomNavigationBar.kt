deebad5481a152f13fd96794415bd64328919917
mehdi <dehghani_m@rocketmail.com>
2023-04-14 14:03:24 +0430

    finish botton navigation bar

---
+++ app/src/main/java/com/example/game_api_compose/navigation/BottomNavigationBar.kt
@@ -0,0 +1,78 @@
+package com.example.game_api_compose.navigation
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.game_api_compose.presentation.theme.ValoLightBlue
+import com.example.game_api_compose.presentation.theme.ValoRed
+import com.example.game_api_compose.presentation.theme.ValoWhite
+
+
+@Composable
+fun BottomNavigationBar(
+    navController: NavController,
+    bottomBarState: MutableState<Boolean>
+) {
+    val items = listOf(
+        BottomNavItem.Agents,
+        BottomNavItem.Maps,
+        BottomNavItem.Weapons,
+        BottomNavItem.Tiers
+    )
+
+    AnimatedVisibility(
+        visible = bottomBarState.value,
+        enter = slideInVertically(initialOffsetY = {it}),
+        exit = slideOutVertically(targetOffsetY = {it}),
+        content = {
+            BottomNavigation {
+                val nevBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentRoute = nevBackStackEntry?.destination?.route
+
+                items.forEach { _ ->
+                    BottomNavigation(
+                        backgroundColor = ValoLightBlue
+                    ) {
+                        items.forEach {item ->
+                            BottomNavigationItem(
+                                icon = {
+                                    Icon(
+                                        painter = painterResource(item.image),
+                                        contentDescription = item.title
+                                    )
+                                },
+                                label = { Text(text = item.title)},
+                                selected = currentRoute == item.route,
+                                onClick = {
+                                    navController.navigate(item.route){
+                                        navController.graph.startDestinationRoute?.let { route ->
+                                            popUpTo(route){
+                                                saveState = true
+                                            }
+                                        }
+                                        launchSingleTop = true
+                                        restoreState = true
+                                    }
+                                },
+                                selectedContentColor = ValoRed,
+                                unselectedContentColor = ValoWhite,
+                                alwaysShowLabel = false
+                            )
+                        }
+                        
+                    }
+                }
+            }
+        }
+    )
+}
