f8949e7cf7e9e1579454b29017999cc4c3bae87f
mehdi <dehghani_m@rocketmail.com>
2023-04-15 13:15:14 +0430

    finish get maps

--- app/src/main/java/com/example/game_api_compose/presentation/map/maps/MapsViewModel.kt
+++ app/src/main/java/com/example/game_api_compose/presentation/map/maps/MapsViewModel.kt
@@ -1,10 +1,34 @@
 package com.example.game_api_compose.presentation.map.maps
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.game_api_compose.common.Resource
+import com.example.game_api_compose.domain.usecase.maps.GetMapsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 @HiltViewModel
 class MapsViewModel @Inject constructor(
+    private val getMapsUseCase: GetMapsUseCase
+) :ViewModel(){
 
-) {
+    private val _state = mutableStateOf(MapsState())
+    val state: State<MapsState> = _state
+
+    init {
+       getMaps()
+    }
+
+    private fun getMaps(){
+        getMapsUseCase().onEach {result ->
+            when(result){
+                Resource.Loading -> _state.value = MapsState(isLoading = true)
+                is Resource.Success -> _state.value = MapsState(maps = result.data)
+                is Resource.Error -> _state.value = MapsState(error = result.errorMessage)
+            }
+
+        }
+    }
 }
