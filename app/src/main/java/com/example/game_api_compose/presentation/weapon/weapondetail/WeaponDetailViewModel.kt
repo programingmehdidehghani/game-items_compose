da89f111bba4a62e575f243a91a80dd1ab48fa7c
mehdi <dehghani_m@rocketmail.com>
2023-04-15 14:58:51 +0430

    make view model

---
+++ app/src/main/java/com/example/game_api_compose/presentation/weapon/weapondetail/WeaponDetailViewModel.kt
@@ -0,0 +1,38 @@
+package com.example.game_api_compose.presentation.weapon.weapondetail
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.game_api_compose.common.Constants
+import com.example.game_api_compose.common.Resource
+import com.example.game_api_compose.domain.usecase.weapons.GetWeaponDetailUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+@HiltViewModel
+class WeaponDetailViewModel @Inject constructor(
+    private val getWeaponDetailUseCase: GetWeaponDetailUseCase,
+    savedStateHandle: SavedStateHandle
+): ViewModel() {
+
+    private val _state = mutableStateOf(WeaponDetailState())
+    val state: State<WeaponDetailState> = _state
+
+    init {
+       savedStateHandle.get<String>(Constants.PARAM_WEAPON_ID)?.let { weaponId->
+           getWeaponDetail(weaponId)
+       }
+    }
+
+    private fun getWeaponDetail(weaponUuid: String){
+         getWeaponDetailUseCase.getWeaponDetailByUuid(weaponUuid).onEach { result ->
+             when(result){
+                 Resource.Loading -> _state.value = WeaponDetailState(isLoading = true)
+                 is Resource.Success -> _state.value = WeaponDetailState(weapon = result.data)
+                 is Resource.Error -> _state.value = WeaponDetailState(error = result.errorMessage)
+             }
+         }
+    }
+}
