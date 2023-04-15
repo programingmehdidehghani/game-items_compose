1c4b98971d5c99d062baa629e66797ceeba04aa4
mehdi <dehghani_m@rocketmail.com>
2023-04-15 13:22:54 +0430

    make map Item

--- app/src/main/java/com/example/game_api_compose/presentation/map/maps/MapItem.kt
+++ app/src/main/java/com/example/game_api_compose/presentation/map/maps/MapItem.kt
@@ -1,2 +1,26 @@
 package com.example.game_api_compose.presentation.map.maps
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.game_api_compose.domain.model.Map
+
+@Composable
+fun MapItem(
+    map: Map,
+    onItemClick: (String) -> Unit
+){
+    Card(
+        modifier = Modifier
+            .clickable { onItemClick(map.uuid.orEmpty()) }
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+
+    }
+
+}
