0fa66e1e45b06bf535bd0adc6390003b015383de
mehdi <dehghani_m@rocketmail.com>
2023-04-15 13:27:45 +0430

    finish map item

--- app/src/main/java/com/example/game_api_compose/presentation/map/maps/MapItem.kt
+++ app/src/main/java/com/example/game_api_compose/presentation/map/maps/MapItem.kt
@@ -1,13 +1,26 @@
 package com.example.game_api_compose.presentation.map.maps
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScopeInstance.align
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.example.game_api_compose.domain.model.Map
+import com.skydoves.landscapist.CircularReveal
+import com.skydoves.landscapist.glide.GlideImage
 
 @Composable
 fun MapItem(
@@ -20,6 +33,34 @@ fun MapItem(
             .fillMaxWidth()
             .padding(16.dp)
     ) {
+        Box(
+            modifier = Modifier.size(width = 200.dp, height = 150.dp)
+        ){
+            GlideImage(
+                imageModel = map.splash,
+                circularReveal = CircularReveal()
+            )
+
+            Text(
+                text = map.displayName.orEmpty(),
+                style = MaterialTheme.typography.h3,
+                color = Color.White,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .background(
+                        brush = Brush.verticalGradient(
+                            listOf(
+                                Color.Transparent,
+                                Color.Gray
+                            )
+                        )
+                    )
+                    .padding(4.dp)
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+        }
 
     }
 
