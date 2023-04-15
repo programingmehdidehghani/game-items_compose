884b390cb9d30b04d19645ba21b7cde9b946e7b8
mehdi <dehghani_m@rocketmail.com>
2023-04-15 15:20:02 +0430

    continu compose weapon detail

--- app/src/main/java/com/example/game_api_compose/presentation/weapon/weapondetail/WeaponDetailScreen.kt
+++ app/src/main/java/com/example/game_api_compose/presentation/weapon/weapondetail/WeaponDetailScreen.kt
@@ -5,13 +5,21 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.game_api_compose.presentation.theme.ValoRed
+import com.skydoves.landscapist.CircularReveal
 import com.skydoves.landscapist.glide.GlideImage
+import com.example.game_api_compose.R
+import com.example.game_api_compose.common.components.HeaderText
+import com.example.game_api_compose.common.components.LinearProgress
+
 
 @Composable
 fun WeaponDetailScreen(
@@ -46,11 +54,61 @@ fun WeaponDetailScreen(
                     verticalArrangement = Arrangement.Center
                 ) {
                     GlideImage(
-                        imageModel =
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 32.dp, vertical = 24.dp),
+                        imageModel = it.displayIcon,
+                        circularReveal = CircularReveal(),
+                        contentDescription = stringResource(R.string.desc_weapon_image)
                     )
 
+                    Spacer(modifier = Modifier.size(12.dp))
+
+                    Text(
+                        text = it.category.orEmpty().replace("EEquippableCategory::",""),
+                        style = MaterialTheme.typography.h5
+                    )
                 }
             }
+            Spacer(modifier = Modifier.size(24.dp))
+            
+            HeaderText(header = stringResource(R.string.title_damage_range))
+
+            Spacer(modifier = Modifier.size(16.dp))
+
+            it.weaponStats?.damageRanges?.getOrNull(0)?.let { damageRange ->
+                Column(
+                    modifier = Modifier.padding(start = 24.dp, end = 24.dp)
+                ) {
+                   damageRange.headDamage?.div(200)?.let { it1 ->
+                       LinearProgress(
+                           header = stringResource(R.string.text_head),
+                           progress = it1.toFloat(),
+                           progressString = damageRange.headDamage.toString()
+                       )
+                   }
+
+                    damageRange.bodyDamage?.toFloat()?.div(200)?.let { it1->
+                        LinearProgress(
+                            header = stringResource(R.string.text_body),
+                            progress = it1,
+                            progressString = damageRange.bodyDamage.toString()
+                        )
+                    }
+
+                    damageRange.legDamage?.div(200)?.let { it1 ->
+                        LinearProgress(
+                            header = stringResource(id = R.string.text_leg),
+                            progress = it1.toFloat(),
+                            progressString = damageRange.legDamage.toString()
+                        )
+                    }
+                }
+            }
+
+            Spacer(modifier = Modifier.size(16.dp))
+            
+            HeaderText(header = )
         }
 
     }
